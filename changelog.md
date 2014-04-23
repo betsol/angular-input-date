e6b18a6b0683657e72f1c2c61d8283d3e4e5a1ba
Slava Fomin II <s.fomin@betsol.ru>
2014-04-23 04:04:31 +0400

    Release version 1.0.1

--- changelog.md
+++ changelog.md
@@ -1,5 +1,10 @@
 # angular-input-date changelog
 
+## Version 1.0.1
+(23 Apr 2014)
+
+- Updated directive to represent input value as a proper Date object
+
 ## Version 1.0.0
 (19 Apr 2014)
 
--- src/angular-input-date.js
+++ src/angular-input-date.js
@@ -3,6 +3,27 @@
 
     var inputDateFormat = 'yyyy-MM-dd';
 
+    /**
+     * Converts string representation of date to a Date object.
+     * @param dateString
+     * @returns {Date|null}
+     */
+    function parseDateString(dateString) {
+        var parts = dateString.split('-');
+        if (3 !== parts.length) {
+            return null;
+        }
+        var year = parseInt(parts[0], 10);
+        var month = parseInt(parts[1], 10);
+        var day = parseInt(parts[2], 10);
+
+        if (month < 1 || year < 1 || day < 1) {
+            return null;
+        }
+
+        return new Date(year, (month - 1), day);
+    }
+
     angular.module('ngInputDate', ['ng'])
         .directive('input', ['dateFilter', function(dateFilter) {
             return {
@@ -20,6 +41,13 @@
                                 dateFilter(ngModel.$viewValue, inputDateFormat)
                             );
                         };
+
+                        element.on('change', function() {
+                            scope.$apply(function() {
+                                var dateString = element.val();
+                                ngModel.$setViewValue(parseDateString(dateString));
+                            });
+                        });
                     }
                 }
             }
